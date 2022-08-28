8d9ff3653db5a87876085b61a1cdb8dafc217e8a
Manas600 <manas.h@somaiya.edu>
2022-08-28 19:36:43 +0530

    Modules
    imported required modules and created imp variables

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import pygame
+import sys
+import pygame.locals 
+
+FPS = 30
+
+screen = pygame.display.set_mode((289, 511))
+groundY = 511 * 0.8
+game_sprite = {}
+game_sound = {}
+
